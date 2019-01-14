d316b4f42b56dbc9cfc1085f2e1a21774e5c2721
Christopher Hopkins <christopher.hopkins@dvla.gov.uk>
2019-01-14 14:52:52 +0000

    417

---
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Category.java
@@ -0,0 +1,25 @@
+package guru.springframework.spring5webfluxrest.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ */
+@Data
+@Document
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    private String id;
+
+    private String description;
+}
