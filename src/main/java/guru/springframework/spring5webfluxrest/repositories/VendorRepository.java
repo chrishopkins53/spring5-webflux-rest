e0a4aafefcb9d9a3ccf7189840b144042df6fe56
Christopher Hopkins <christopher.hopkins@dvla.gov.uk>
2019-06-12 14:22:10 +0100

    421

--- src/main/java/guru/springframework/spring5webfluxrest/repositories/VendorRepository.java
+++ src/main/java/guru/springframework/spring5webfluxrest/repositories/VendorRepository.java
@@ -1,8 +1,10 @@
 package guru.springframework.spring5webfluxrest.repositories;
 
 import guru.springframework.spring5webfluxrest.domain.Vendor;
-
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
+/**
+ *
+ */
 public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
 }
