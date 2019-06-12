0ed686c278c471ec38cb06e875590ea3a9ee2614
Christopher Hopkins <christopher.hopkins@dvla.gov.uk>
2019-06-12 15:33:04 +0100

    423

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/CategoryController.java
@@ -5,8 +5,14 @@ import guru.springframework.spring5webfluxrest.repositories.CategoryRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +35,17 @@ public class CategoryController {
     @GetMapping("/api/v1/categories/{id}")
     Mono<Category> getById(@PathVariable String id){
         return categoryRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/categories")
+    Mono<Void> create(@RequestBody Publisher<Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream).then();
+    }
+
+    @PutMapping("/api/v1/catgories/{id}")
+    Mono<Category> update(@PathVariable String id, @RequestBody Category category) {
+        category.setId(id);
+        return categoryRepository.save(category);
     }
 }
