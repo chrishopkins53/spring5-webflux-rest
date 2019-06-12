d2b5537c565cdb18a04d74817654509df39fbb0e
Christopher Hopkins <christopher.hopkins@dvla.gov.uk>
2019-06-12 15:42:18 +0100

    425

--- src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
+++ src/main/java/guru/springframework/spring5webfluxrest/controllers/VendorController.java
@@ -5,8 +5,14 @@ import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -29,5 +35,11 @@ public class VendorController {
     @GetMapping("/api/v1/vendors/{id}")
     Mono<Vendor> getById(@PathVariable String id) {
         return vendorRepository.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/api/v1/vendors")
+    Mono<Void> create(@RequestBody Publisher<Vendor> vendorStream) {
+        return vendorRepository.saveAll(vendorStream).then();
     }
 }
